a29448415e9c058fac4c927c34dd0f3ebc7b9192
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2022-08-25 11:06:59 -0300

    aula 3 pesquisa e ordenacao

--- aula sort 2/main.cpp
+++ aula sort 2/main.cpp
@@ -6,18 +6,26 @@
 
 using namespace std;
 
-#define TAM 6
+#define TAM 500
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-    
+
+
     int vet[TAM];
 
     fill_vet(vet, TAM);
     
+    cout << "Antes do sort" << endl;
     show_vet(vet, TAM);
+    cout << "################" << endl;
 
-    
+    cout << endl << endl;
+
+    sort_comb(vet, TAM);
+
+    cout << endl << "Fim do sort\n\n\n";
+    show_vet(vet, TAM);
 
     return 1;
 
