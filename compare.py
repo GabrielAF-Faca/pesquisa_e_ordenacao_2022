226239e3c3c2860ac21052dd85f39d0817ed6464
Gabriel Azenha Fachim <101106957+GabrielAF-Faca@users.noreply.github.com>
2022-10-20 11:16:30 -0300

    Add files via upload

--- compare.py
+++ compare.py
@@ -8,11 +8,9 @@ class Aluno:
             return self.matricula == other.matricula
         return False
 
-aluno1 = Aluno(1, "Nome1")
-aluno2 = Aluno(2, "Nome2")
 
-lista = [aluno1, aluno2]
+lista = [Aluno(1, "Nome1"), Aluno(2, "Nome2")]
 
-alunoTeste = Aluno(2, "Nome2")
+alunoTeste = Aluno(1, "Nome2")
 
 print(lista.index(alunoTeste))
